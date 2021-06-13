8482e24a13d1afd54bd3d75a373a391a1f27e8e1
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-06-13 23:19:23 +0200

    chore(swagger): remove temporarily

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import swaggerUi from "swagger-ui-express";
 import router from "./routes";
 
 const app = express();
@@ -12,15 +11,5 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
 
 app.use(router);
-
-app.use(
-  "/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
-  })
-);
 
 export default app;
--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -1,14 +1,10 @@
-import { Route, Get, Tags } from "tsoa";
 import {
   formatPhoneNumber,
   fetchCompanyPhoneNumber,
   ICompanyParams,
 } from "../repositories/company";
 
-@Route("api/company")
-@Tags("Company")
 export class CompanyController {
-  @Get("/")
   public async fetchCompanyPhoneNumber(
     params: ICompanyParams
   ): Promise<string> {
